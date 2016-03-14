8aa49e944cb4a561cb9d2caa363adb32218a9a15
yibuyisheng <yibuyisheng@163.com>
2016-03-14 21:35:36 +0800

    注释

--- src/BaseView.js
+++ src/BaseView.js
@@ -6,14 +6,30 @@ define(function (require) {
     return Backbone.View.extend({
         Model: Backbone.Model,
 
-        // 初始化的模板数据
+        /**
+         * 初始化的模板数据，结构大致为
+         * {
+         *     key1: [*],
+         *     key2: [*],
+         *     ...
+         * }
+         *
+         * @protected
+         * @type {Map.<string, *>} 初始数据
+         */
         templateData: {},
-        // 模板里面用到的回调函数，注意这些回调函数都会自定绑定this到当前view实例
-        templateCallbacks: {},
-
         getTemplateData: function getTemplateData() {
             return this.templateData;
         },
+
+        /**
+         * 模板里面用到的回调函数，注意这些回调函数都会自定绑定this到当前view实例。
+         * 如果value是字符串的话，则在当前view实例中找相应的方法。
+         *
+         * @protected
+         * @type {Map.<string, string|function>}
+         */
+        templateCallbacks: {},
         getTemplateCallbacks: function getTemplateData() {
             for (var key in this.templateCallbacks) {
                 if (u.isString(this.templateCallbacks[key])) {
@@ -26,7 +42,7 @@ define(function (require) {
             }
 
             u.forEach(this.templateCallbacks, function (callback, key) {
-                 this.templateCallbacks[key] = u.bind(callback, this);
+                this.templateCallbacks[key] = u.bind(callback, this);
             }, this);
 
             return this.templateCallbacks;
@@ -49,12 +65,22 @@ define(function (require) {
             this.context = options.context;
         },
 
-        // 是否能够进入view。此处可以做一些检查，比如是否登陆等等。
+        /**
+         * 是否能够进入view。此处可以做一些检查，比如是否登陆等等。
+         * 在子类中可以覆盖这个方法。
+         *
+         * @public
+         * @return {Promise.<boolean>|boolean}
+         */
         canEnter: function canEnter() {
             return true;
         },
 
-        // 进入
+        /**
+         * 进入view，开始渲染。
+         *
+         * @public
+         */
         enter: function () {
             var options = this.$$options;
 
